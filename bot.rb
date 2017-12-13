ec0746971c3ea91f2533dc51d04f88b3245be0d6
Manu Wallner <manu@supermil.ch>
2017-12-13 21:56:17 +0100

    Don’t notify slack groups on weekends
    Nobody reads these messages on the weekend anyway. Regressions will still be reported on the weekend!

--- bot.rb
+++ bot.rb
@@ -27,6 +27,12 @@ module Fastlane
 
     attr_reader :logger
 
+    def self.should_notify_slack_message_not_that_important_though?
+      return true unless Date.today.saturday? || Date.today.sunday?
+      logger.info("Not notifying anyone because it's the weekend and people should enjoy their time off")
+      return false
+    end
+
     def initialize(logger)
       @logger = logger
     end
@@ -186,6 +192,7 @@ module Fastlane
 
     def notify_action_channel_about(needs_attention_prs)
       return unless needs_attention_prs.any?
+      return unless Bot.should_notify_slack_message_not_that_important_though?
 
       logger.info("Notifying the Slack room about PRs that need attention...")
 
